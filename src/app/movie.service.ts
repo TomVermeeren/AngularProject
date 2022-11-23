957def068dedfbce06ba1ed3a1f101c4ed63d10b
Tom Vermeeren <r0832730@student.thomasmore.be>
2022-11-23 11:42:42 +0100

    trending movies op watchlist gezet, als volgende card maken en op homepage zetten en dan zoekfunctie invoegen

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,31 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Movie } from './movie';
+import { HttpClient } from '@angular/common/http';
+import { Observable, switchMap, timer } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  movies: Movie[] = [];
 
-  getMovies(): Movie[] {
-    let movies: Movie[] = [];
-
-    let movie1: Movie = {
-      id: 1,
-      imdb_id: 235,
-      genres: [],
-      title: "test motj",
-      overview: "overview",
-      popularity: "popularity",
-      poster_path: "poster_path",
-      release_date: "release_date",
-    };
-
-
-    movies.push(movie1);
-
-    return movies;
+  api_key = "206819812f207cb0e7f60c698ee380a1"
+  constructor(private httpClient: HttpClient) {
   }
 
+  getMovies(): Observable<any> {
+    return this.httpClient.get<any>("https://api.themoviedb.org/3/movie/popular?api_key=" + this.api_key);
+  }
+
+  getMovieById(id: number): Observable<any> {
+    return timer(1, 3000).pipe(switchMap(() => this.httpClient.get<any>("https://api.themoviedb.org/3/movie/popular?api_key=8a31694e372bf87df346f81ef6bd7705&language=en-US&page=1%22")));
+  }
 }
